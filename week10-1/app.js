bf717a3733e85fb48897216b76b972e7b7cd3be3
Oliy <74090200+eunsoA@users.noreply.github.com>
2024-11-08 10:37:31 +0900

    update 10-2 실습1

--- week10-1/app.js
+++ week10-1/app.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config(); // .env 파일에서 환경 변수 로드
 
 const app = express();
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 3000);
+
+// 공통 미들웨어 설정
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
+
 
 app.use((req, res, next) => {
     console.log('모든 요청에 다 실행됩니다.');
@@ -11,12 +36,12 @@ app.use((req, res, next) => {
 app.get('/', (req, res, next) => {
     console.log('GET / 요청에서만 실행됩니다.');
     next();
-}, (req, res, next) => {
+}, (req, res) => {
     throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
 });
 
 app.use((err, req, res, next) => {
-    console.error(err);
+    console.error(err.message);
     res.status(500).send(err.message);
 });
 
