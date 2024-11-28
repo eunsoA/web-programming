b80a34d9999ed9fc2c23492871edaa6204dead74
Oliy <eunsoa0310@gmail.com>
2024-11-28 12:00:49 +0900

    update week12-2 part 3
    * 카카오 부분 지워야함.

--- nodebird/routes/page.js
+++ nodebird/routes/page.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const { renderProfile, renderJoin, renderMain } = require('../controllers/page');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.locals.user = null;
+  res.locals.user = req.user;
   res.locals.followerCount = 0;
   res.locals.followingCount = 0;
   res.locals.followingIdList = [];
   next();
+});
+
+router.get('/profile', isLoggedIn, (req, res) =>{
+  res.render('profile', {title: '내 정보-NodeBird'});
+});
+
+router.get('./join', isNotLoggedIn, (req, res) => {
+  res.render('join', {title: '회원가입 - NodeBird'});
 });
 
 router.get('/profile', renderProfile);
